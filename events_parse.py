72d9c63a5e3fd6cb634be8c0962ce80d5dd64901
thestrider3 <tulikabhatt92@gmail.com>
2016-03-12 21:23:46 -0500

    comments added

--- events_parse.py
+++ events_parse.py
@@ -1,3 +1,4 @@
+##The code snippets were taken from E6998:Storytelling with Streaming Data class. In this file, we are processing the response obtained from curling the meetup stream. Two redis databases are used. One is used for storing the timestamp of the incoming stream for calculating the rate of the stream. The other database is used to store the number of people attending an event of a particular category in the city.
 import json
 import sys
 import time
@@ -40,11 +41,13 @@ while 1:
         # if there is no status present in the message
         # then let's just ditch it
         continue 
-    
+
+####take only those events which are upcoming, ignore the canceled ones 
+   
     if status=='upcoming':     
 	    conn1.hincrby(city, category, members)
 	    print json.dumps({"city":city,"category":category, "members":members, "status":status})
-
+###increase the hash value of each category by the number of people attending that event
 	    t=float(time.time())
 	    if lastArrival==0:
 		lastArrival=t
@@ -52,6 +55,8 @@ while 1:
 	    delta=t-lastArrival
 	    lastArrival=t
 	    rate=1/delta
+###calculating rate as the inverse of the difference between the timestamp of two incoming stream event
 	    conn0.setex(t, rate, 120)
+####these values of rates are expired after 120 seconds so that the rate reflects the current rate of the stream rather than some past value
 	    print json.dumps({"time":t, "rate": rate, "delta":delta})
 	    sys.stdout.flush()
