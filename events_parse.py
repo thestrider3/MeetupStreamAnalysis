f38fa4e12dccb860e5536db943ebb9fa3ae5107a
thestrider3 <tulikabhatt92@gmail.com>
2016-03-12 21:14:40 -0500

    updated upcoming

--- events_parse.py
+++ events_parse.py
@@ -24,27 +24,34 @@ while 1:
     try:
         category = d['group']['category']['name']
     except KeyError:
-        # if there is no referrer present in the message
+        # if there is no category defined in the message
         # then let's just ditch it
         continue
 
     try:
         members = d['yes_rsvp_count']
     except KeyError:
-        # if there is no referrer present in the message
+        # if there is no members who have rsvped yes in the message
         # then let's just ditch it
         continue 
-     
-    conn1.hincrby(city, category, members)
-    print json.dumps({"city":city,"category":category, "members":members})
+    try:
+        status = d['status']
+    except KeyError:
+        # if there is no status present in the message
+        # then let's just ditch it
+        continue 
+    
+    if status=='upcoming':     
+	    conn1.hincrby(city, category, members)
+	    print json.dumps({"city":city,"category":category, "members":members, "status":status})
 
-    t=float(time.time())
-    if lastArrival==0:
-	lastArrival=t
-	continue
-    delta=t-lastArrival
-    lastArrival=t
-    rate=1/delta
-    conn0.setex(t, rate, 120)
-    print json.dumps({"time":t, "rate": rate, "delta":delta})
-    sys.stdout.flush()
+	    t=float(time.time())
+	    if lastArrival==0:
+		lastArrival=t
+		continue
+	    delta=t-lastArrival
+	    lastArrival=t
+	    rate=1/delta
+	    conn0.setex(t, rate, 120)
+	    print json.dumps({"time":t, "rate": rate, "delta":delta})
+	    sys.stdout.flush()
