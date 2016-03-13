c4639b2768450854b0c1f3591325c7ea73bd0adb
thestrider3 <tulikabhatt92@gmail.com>
2016-03-12 19:42:54 -0500

    Streaming Meetup added

---
+++ events_parse.py
@@ -0,0 +1,50 @@
+import json
+import sys
+import time
+import redis
+conn1 = redis.Redis(db=1)
+lastArrival=0
+conn0 = redis.Redis(db=0)
+
+while 1:
+    line = sys.stdin.readline()
+    try:
+        d = json.loads(line)
+    except ValueError:
+        # sometimes we get an empty line, so just skip it
+        continue
+
+    try:
+        city = d['venue']['city']
+    except KeyError:
+        # if there is no city present in the message
+        # then let's just ditch it
+        continue
+
+    try:
+        category = d['group']['category']['name']
+    except KeyError:
+        # if there is no referrer present in the message
+        # then let's just ditch it
+        continue
+
+    try:
+        members = d['yes_rsvp_count']
+    except KeyError:
+        # if there is no referrer present in the message
+        # then let's just ditch it
+        continue 
+     
+    conn1.hincrby(city, category, members)
+    print json.dumps({"city":city,"category":category, "members":members})
+
+    t=float(time.time())
+    if lastArrival==0:
+	lastArrival=t
+	continue
+    delta=t-lastArrival
+    lastArrival=t
+    rate=1/delta
+    conn0.setex(t, rate, 120)
+    print json.dumps({"time":t, "rate": rate, "delta":delta})
+    sys.stdout.flush()
