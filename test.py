4ce947e7d837f3df7e4c4ec6bac1434f022ce03e
thestrider3 <tulikabhatt92@gmail.com>
2016-03-12 21:46:48 -0500

    comments added

--- test.py
+++ test.py
@@ -1,4 +1,6 @@
-
+#The code snippets were taken from E6998:Storytelling with Streaming Data class. In this file, I have created an alerting system.
+#I am monitoring the entropy of the stream; if the entropy changes by 0.001, then a tweet is posted through my account. This entropy 
+#change signifies that more people are now going to events based on category which were less popular.
 import redis
 import json
 import time
@@ -32,8 +34,7 @@ while 1:
         h={k:v/float(z) for k,v in dict.iteritems()}
         entropy=-sum([p*np.log(p) for p in h.values()])
         entropy_diff=entropy-entropy_prev
-        entropy_prev=entropy
-        if (entropy_diff>0.0001):
+        if (entropy_diff>0.001 and int(entropy_prev)!=0):
             time.sleep(5)
             auth=tweepy.OAuthHandler('Consumer Key','Consumer Sceret Key')
             auth.set_access_token('Access Key','Access Secret Key')
@@ -42,4 +43,4 @@ while 1:
             count=count+1
             tweet = "Entropy changed "+str(count)+"by "+str(entropy_diff)
             status = api.update_status(status=tweet)
-        
+        entropy_prev=entropy
